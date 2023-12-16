3375e295e219813dad76e7b4e40f22c3e2bdb791
BlueJCash <106276517+BlueJCash@users.noreply.github.com>
2023-12-15 21:35:37 -0700

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 from keras.preprocessing.image import ImageDataGenerator
 
-# Load the test dataset
+# Loads the dataset. Change the path to match the location of the sample dataset
 external_drive_path = 'D:\\sun397_testset'
 
 datagen = ImageDataGenerator(rescale=1./255)
@@ -13,10 +13,10 @@ test_gen = datagen.flow_from_directory(
     class_mode='sparse'
 )
 
-# Load the saved model
+# Loads the saved model
 loaded_model = tf.keras.models.load_model('sun397_model')
 
-# Evaluate the loaded model on the test dataset
+# Evaluates the loaded model on the test dataset
 evaluation = loaded_model.evaluate(test_gen)
 print("Test Accuracy: {:.2f}%".format(evaluation[1] * 100))
  
